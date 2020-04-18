1bf180aa49a86b6515cbfbfe9242199bce82af48
mhmod <mh_mouds@hotmail.com>
2020-04-18 20:22:51 +0300

    last

--- TestOutlook.py
+++ TestOutlook.py
@@ -1,36 +1,42 @@
 import win32com.client as win32
 from datetime import datetime
 from datetime import timedelta
+import time
 
 outlook = win32.Dispatch('outlook.application').GetNamespace("MAPI")
 #Data=outlook.mailItem
 inbox  = outlook.GetDefaultFolder(6)
-messages = inbox.Items
+root_folder = inbox.Folders(4)
+messages = root_folder.Items
 
 
-currentDT = datetime.now()
-print (str(currentDT))
-currentDT=currentDT-timedelta(minutes=20)
+print(root_folder.name)
+while (True):
+    messages = inbox.Items
+    currentDT = datetime.now()
+    print (str(currentDT))
+    currentDT=currentDT-timedelta(minutes=20)
 
-print (currentDT.strftime("%m/%d/%Y %H:%M:%S"))
+    print (currentDT.strftime("%m/%d/%Y %H:%M:%S"))
 
-message=messages.find("[ReceivedTime] >'"+ str(currentDT.strftime("%m/%d/%Y %H:%M"))+"' And [From]='Mahmoud Sayid'")
+    message=messages.find("[ReceivedTime] >'"+ str(currentDT.strftime("%m/%d/%Y %H:%M"))+"' And [From]='Mahmoud Sayid' And [Subject]='& QUANTA &'")
 
 
 
-#message = messages.GetFirst ()
-try:
-    received_time = str(message.ReceivedTime)
-    print  (message.Subject+"time "+received_time )
-    messages.Sort 
-    message = messages.GetFirst ()
-    received_time = str(message.ReceivedTime)
-    print  (message.Subject+"time "+received_time )
+    #message = messages.GetFirst ()
+    try:
+        received_time = str(message.ReceivedTime)
+        print  (message.Subject+"time "+received_time )
+        messages.Sort 
+        message = messages.GetFirst ()
+        received_time = str(message.ReceivedTime)
+        print  (message.Subject+"time "+received_time )
 
-    print ('total messages: ', len(messages))
+        print ('total messages: ', len(messages))
 
-except Exception as err:
-    print(err)
+    except Exception as err:
+        print(err)
+    time.sleep(2.4)
 
 
 
